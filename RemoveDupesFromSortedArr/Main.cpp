55a27b3972529029175ab6b2c5487c44419d129d
Fibonacci <dave.green1664@yahoo.co.uk>
2021-07-30 17:13:02 +0100

    Simple solution using the std library. Surprisingly it took me a long time of messing around with crazy ideas before I decided to research and I'm starting to realise that if you were to know the std lib inside and out, then a lot of tasks can be made significantly simpler.

--- RemoveDupesFromSortedArr/Main.cpp
+++ RemoveDupesFromSortedArr/Main.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
+#include <vector>
+#include <algorithm>
+
+int removeDuplicates(std::vector<int>& nums)
+{
+    // Create an iterator to hold the new off the end pos
+    std::vector<int>::iterator it;
+
+    // Remove all non-unique values
+    it = std::unique(nums.begin(), nums.end());
+
+    // Resize the vector to the new size
+    nums.resize(std::distance(nums.begin(), it));
+
+    return nums.size();
+}
 
 int main()
 {
+    // TEST CASES
+    std::vector<int> case1{ 0,0,0,1,1,2,2,2,3,4,5 };
+
+    int ans = removeDuplicates(case1);
+
+    std::cout << "ans = " << ans << '\n';
+
 	return 0;
 }
