1acd4dde46ae4f64ce82af4ffdbd29ecc3b902f2
Fibonacci <dave.green1664@yahoo.co.uk>
2021-07-31 12:53:26 +0100

    Created an alt solution with a slow-runner as per the provided online solution in order to undersand how this was solved WITHOUT using the std lib. Although I#m still not sure under what circumstance one would ever be in that they would not be able to use the std lib and be forced to piece together such a solution. I also resized at the end of the alt, whereby the provided solution did NOT, this is because the problem states that duplicates must be removed, without this the duplicates are simply move to the back of the vector.

--- RemoveDupesFromSortedArr/Main.cpp
+++ RemoveDupesFromSortedArr/Main.cpp
@@ -16,12 +16,45 @@ int removeDuplicates(std::vector<int>& nums)
     return nums.size();
 }
 
+// Alt
+int altRemoveDuplicates(std::vector<int>& nums)
+{
+    if (nums.size() == 0)
+    {
+        return 0;
+    }
+
+    // Create var to point to slow runner
+    int i = 0;
+
+    // Create loop starting at 2nd index pos to loop over entire vector
+    for (int j = 1; j < nums.size(); ++j)
+    {
+        // Keep looping until we find values that don't match
+        if (nums[j] != nums[i])
+        {
+            // Move the slow runner to the next index and swap out the
+            // value at slow runner pos with the value that is NOT a duplicate
+            ++i;
+            nums[i] = nums[j];
+        }
+    }
+
+    // Resize the nums vector to REMOVE the duplicates
+    // as the problem states duplicates must be REMOVED
+    // Without this they are simply pushed to the back
+    nums.resize(i + 1);
+
+    return nums.size();
+}
+
 int main()
 {
     // TEST CASES
     std::vector<int> case1{ 0,0,0,1,1,2,2,2,3,4,5 };
 
-    int ans = removeDuplicates(case1);
+    //int ans = removeDuplicates(case1);
+    int ans = altRemoveDuplicates(case1);
 
     std::cout << "ans = " << ans << '\n';
 
